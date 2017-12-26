3f74480a2f71517620570e082ad6f741d2cd5acb
Gerry Mc Donnell <irishbloke@gmail.com>
2017-12-26 17:32:29 +0000

    part 3 done

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,15 +1,12 @@
-from django.shortcuts import render
-from django.template import loader
 from django.http import Http404
-
+from django.template import loader
 from .models import Question
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404,render
 
 # Create your views here.
-from django.http import HttpResponse
-
-
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.order_by('-pub_date')[:2]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 	
