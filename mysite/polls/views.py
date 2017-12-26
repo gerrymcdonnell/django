1fe7bc46ab489d5cbe241d949d2ab00916a57c0a
Gerry Mc Donnell <irishbloke@gmail.com>
2017-12-26 16:41:10 +0000

    switched to py 3.6

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -13,11 +13,9 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 	
+# ...
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
